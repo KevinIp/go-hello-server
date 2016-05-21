3aa340d8f40933de4097681f3c32f9300375c022
KevinIp <delaplace2000@yahoo.com>
2016-05-21 10:01:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# go-hello-server
+
+http server example in Go
+
+## to start
+
+```
+./run.sh
+```
+
+goto browser
+```
+http://localhost:8080?name=world
+```
+
+Enjoy
---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+    fmt.Fprintf(w, "Hello, %s!", name)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
